42cd18319b437958d02b881b6a96f47f57d47c44
Alex Bispo de Oliveira <alexbispo.artes@gmail.com>
2024-01-31 14:29:59 -0300

    Formating the money values.

--- frontend/canary-data-insiders/src/components/TradingRow/index.js
+++ frontend/canary-data-insiders/src/components/TradingRow/index.js
@@ -1,13 +1,34 @@
+import "intl";
+import "intl/locale-data/jsonp/en-US"
+
 export default function TradingRow({ trading }) {
+  
+  function formatMoney(amount) {
+    if (amount) {
+      return Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD"
+      }).format(amount);
+    }
+    return "N/A";
+  }
+
+  function formatPercent(value) {
+    if (value) {
+      return `${value}%`;
+    }
+    return "N/A";
+  }
+
+
   return (
     <tr>
       <td>{trading.ticker}</td>
       <td>{trading.name}</td>
       <td>{trading.startDate}</td>
       <td>{trading.shares}</td>
-      <td>{trading.amount}</td>
-      <td>{trading.market_cap_percentage}</td>
-      <td>{trading.market_cap}</td>
+      <td>{formatMoney(trading.amount)}</td>
+      <td>{formatPercent(trading.market_cap_percentage)}</td>
+      <td>{formatMoney(trading.market_cap)}</td>
     </tr>
   );
 }
